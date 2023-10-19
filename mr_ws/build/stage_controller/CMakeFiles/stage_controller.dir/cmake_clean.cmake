4e1b6cde85965e1f556aab171fde274d39660136
DoneZai <gybgyjl@qq.com>
2023-10-19 19:45:56 +0300

    finish hw2

---
+++ mr_ws/build/stage_controller/CMakeFiles/stage_controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/jinlong/local/workspace/mr_ws/devel/lib/stage_controller/stage_controller"
+  "/home/jinlong/local/workspace/mr_ws/devel/lib/stage_controller/stage_controller.pdb"
+  "CMakeFiles/stage_controller.dir/src/stage_controller.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stage_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
